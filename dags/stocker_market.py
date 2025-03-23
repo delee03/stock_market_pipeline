a2da4f7ba566eec0985fdab98a2f3975efe984df
fuderrpham <128588793+delee03@users.noreply.github.com>
2025-03-23 23:38:42 +0700

    lines 2: fetching stock prices with Python Operator

--- dags/stocker_market.py
+++ dags/stocker_market.py
@@ -1,8 +1,12 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
+from airflow.operators.python import PythonOperator 
 from airflow.sensors.base import PokeReturnValue
 from datetime import datetime
 import requests
+from include.stock_market.tasks import _get_stock_prices
+
+SYMBOL="nvda"
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -23,7 +27,12 @@ def stock_market():
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition, xcom_value=url)
     
-    is_api_available()
+    getStockPrices = PythonOperator(
+        task_id = "get_stock_prices",
+        python_callable=_get_stock_prices,
+        op_kwargs={'url': '{{ti.xcom_pull(task_ids="is_api_available")}}','symbol': SYMBOL}
+    )
+    is_api_available() >> getStockPrices
         
 
 stock_market()
